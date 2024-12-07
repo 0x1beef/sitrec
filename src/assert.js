f5b3fbabff93a3c38e4cce5d5fad9171e2796a4e
0x1beef <156372403+0x1beef@users.noreply.github.com>
2024-12-07 13:55:07 -0500

    Halt execution on assert fail in console mode
    Make sure the tests fail if any of the asserts fail in console mode.

--- src/assert.js
+++ src/assert.js
@@ -1,5 +1,12 @@
+import {isConsole} from "../config"
+
 export function assert(condition, message = false) {
     if (!condition) {
+        if (isConsole) {
+            // bun ignores the 'debugger' keyword and continues execution
+            // so make sure assert failures are caught during the tests
+            throw new Error("ASSERT: " + message)
+        }
         console.trace()
         console.error("ASSERT: " + message);
         debugger;
